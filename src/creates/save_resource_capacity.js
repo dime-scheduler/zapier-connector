c03b5fe02fe2b99efdd488bc5926045769139eed
Hendrik Bulens <hendrik.bulens@gmail.com>
2022-10-27 10:32:07 +0200

    Omit empty fields

--- src/creates/save_resource_capacity.js
+++ src/creates/save_resource_capacity.js
@@ -1,32 +1,20 @@
-const headers = require('../utils/auth.js');
+const { createAction } = require('../utils/actionBuilder.js');
 const constants = require('../utils/constants.js');
-const { createUri, createFields, createBody } = require('../utils/actionHelpers.js');
 
 const fields = [
   { key: 'resourceNo', label: 'ResourceNo', type: "string", required: false },
-  { key: 'date', label: 'Date', type: "string", required: false },
-  { key: 'capacityInSeconds', label: 'CapacityInSeconds', type: "string", required: false },
+  { key: 'date', label: 'Date', type: "datetime", required: false },
+  { key: 'capacityInSeconds', label: 'CapacityInSeconds', type: "integer", required: false },
   { key: 'quantity', label: 'Quantity', type: "string", required: false },
   { key: 'unitOfMeasure', label: 'UnitOfMeasure', type: "string", required: false },
   { key: 'unitOfMeasureConversion', label: 'UnitOfMeasureConversion', type: "string", required: false },
 ];
 
-module.exports = {
-  key: 'save_resource_capacity',
-  noun: 'Resource Capacity',
-  display: {
-    label: 'Save resource capacity',
-    description: 'Saves a capacity entry for a resource',
-    hidden: false,
-    important: true,
-  },
-  operation: {
-    inputFields: createFields(fields),
-    perform: {
-      url: createUri(constants.endpoints.resource.capacity),
-      method: 'POST',
-      headers,
-      body: createBody(fields),
-    },
-  },
-};
+module.exports = createAction(
+  constants.endpoints.resource.capacity,
+  'save_resource_capacity',
+  'Resource Capacity',
+  'Save resource capacity',
+  'Saves a resource capacity.',
+  fields
+);
